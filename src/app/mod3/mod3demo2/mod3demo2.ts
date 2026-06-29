bf3373a87f95fefaf935964d652f2a4c33802e87
stropee <stropee@campus-eni.fr>
2026-06-29 15:04:32 +0200

    module 3 demo 2

--- src/app/mod3/mod3demo2/mod3demo2.ts
+++ src/app/mod3/mod3demo2/mod3demo2.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 import {FormsModule} from '@angular/forms';
+import {DatePipe, UpperCasePipe} from '@angular/common';
+
+import localeFr from '@angular/common/locales/fr';
+import {registerLocaleData} from '@angular/common';
+registerLocaleData(localeFr);
 
 @Component({
   selector: 'app-mod3demo2',
   imports: [
-    FormsModule
+    FormsModule,
+    UpperCasePipe,
+    DatePipe
   ],
   templateUrl: './mod3demo2.html',
   styleUrl: './mod3demo2.css',
@@ -13,10 +20,12 @@ export class Mod3demo2 {
 
   public user : string;
   public users : string[] // Array<string>
+  public date : Date;
 
   constructor() {
     this.user = '';
     this.users = [];
+    this.date = new Date();
   }
 
   public addUser(){
